d0fbc5c27a0f5ca90bc7e84931ac5ce6cf8859b4
Nin-jaco <jaco.kleynhans@icloud.com>
2020-09-10 19:25:50 +0200

    ReactNative Exercise files

---
+++ ReactNative/Ch02/02_02/App.js
@@ -0,0 +1,16 @@
+// This is the code that we typed into Expo Snack.
+// You can view this running here: https://snack.expo.io/h6bFTzw!u
+
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <Text>Hello World</Text>
+      <Text>Red</Text>
+      <Text>Green</Text>
+      <Text>Blue</Text>
+    </View>
+  );
+}
